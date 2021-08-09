5c7917b101266fbd2bf82d5f8afc33c7409d966b
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-08-09 14:50:46 +1000

    Add lecture 3 functions demos

---
+++ lecture_03.py
@@ -0,0 +1,35 @@
+"""CP1404 lecture 03"""
+
+from string import ascii_letters
+
+
+def main():
+    """Demonstrate line functions."""
+    length_of_line = int(input("How many? "))
+    print_line(length_of_line)
+    print(print_line(20))
+
+
+def print_line(length):
+    """Print a line of length hyphens."""
+    print(length * '-')
+
+
+def count_letters(string):
+    count = 0
+    for character in string:
+        if character in ascii_letters:
+            count += 1
+    return count
+
+
+def test_count():
+    assert count_letters("CP1404") == 2
+    assert count_letters("1404 a 983") == 1
+    # print(count_letters("CP1401") > count_letters("CP1404"))
+
+
+test_count()
+
+# main()
+
