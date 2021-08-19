9e5e71085470efef8e4c6ad515ffbae33dd845ea
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-08-19 14:32:57 +1000

    Add lecture 4 lists demos

--- lecture_04.py
+++ lecture_04.py
@@ -8,3 +8,15 @@ for character in name:
     if character.lower() in VOWELS:
         number_of_vowels += 1
 print(f"Out of {len(name)} letters, {name} has {number_of_vowels} vowels")
+
+# make names title case
+names = ["Bob", 'jimbo', 'saLLy']
+for name in names:
+    name = name.title()
+for i in range(len(names)):
+    names[i] = names[i].title()
+print(names)
+names2 = []
+for name in names:
+    names2.append(name.title())
+print(names2)
