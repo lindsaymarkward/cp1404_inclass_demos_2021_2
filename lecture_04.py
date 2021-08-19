7900ab228c89afda6025efbeee049cd1c9d2689d
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-08-19 15:04:33 +1000

    Finalise and add comments to lecture 4 lists demos including comprehensions

--- lecture_04.py
+++ lecture_04.py
@@ -9,14 +9,34 @@ for character in name:
         number_of_vowels += 1
 print(f"Out of {len(name)} letters, {name} has {number_of_vowels} vowels")
 
+# Strings in a list
 # make names title case
 names = ["Bob", 'jimbo', 'saLLy']
+print(names)
+
+# Why doesn't this work?
 for name in names:
     name = name.title()
+print(names)
+
+# But this does?
 for i in range(len(names)):
     names[i] = names[i].title()
 print(names)
-names2 = []
+
+# enumerate is good when we need both the index and the element
+for i, name in enumerate(names):
+    names[i] = name.title()
+print(names)
+
+# Let's make a new list
+new_names = []
 for name in names:
-    names2.append(name.title())
-print(names2)
+    new_names.append(name.title())
+print(new_names)
+
+# Or we could achieve the same in one line with a list comprehension :)
+new_names = [name.title() for name in names]
+
+# We could use a list comprehension to make other things, like the lengths of the names
+lengths = [len(name) for name in names]
