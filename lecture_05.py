65d07272e1d8d0f9a611b329e6e6889b3e6e10a9
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-08-23 14:37:22 +1000

    Fix lecture 5 DTN based on code review; add docstrings and variables

--- lecture_05.py
+++ lecture_05.py
@@ -4,13 +4,17 @@ This is a new fun thing of happiness.
 
 
 def main():
+    """Lecture 5 demo on lists in functions."""
     names = ["Alice", "Bob", "Carly", "David", "Lindsay"]
     ages = [3, 20, 7, 1, 21]
     print(determine_oldest(names, ages))
 
 
 def determine_oldest(names, ages):
-    return names[ages.index(max(ages))]
+    """Determine the oldest name based on parallel lists."""
+    oldest_age = max(ages)
+    index = ages.index(oldest_age)
+    return names[index]
 
 
 if __name__ == '__main__':
